decbe1ed374da32ce1c9ff465bcc69c944334580
Mohamed <M.Mahgoub04@gmail.com>
2022-05-02 01:45:14 +0400

    Add password visibility

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 
 document.addEventListener('DOMContentLoaded', () => {
+ const eye1 = document.querySelector('#eye1')
+ const pass1 = document.querySelector('#pass1')
+ var passHidden1 = true
 
+ eye1.onclick = () => {
+  if(passHidden1){
+   passHidden1 = false
+   eye1.setAttribute('src', 'visible.svg')
+   pass1.type = 'text'
+  } else if (!passHidden1){
+   passHidden1 = true
+   eye1.setAttribute('src', 'hidden.svg')
+   pass1.type = 'password'
+  }
+ }
 
 
 }) 
